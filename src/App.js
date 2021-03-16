a49d03eb6e606a836e13b0391c68cae2c207be52
Riaj Ahmed <riajahmedras@gmail.com>
2021-03-16 21:30:14 +0600

    Show name, password, email

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () =>{
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    const {displayName, photoURL, email} = result.user;
+    const signedInUser = {
+      isSignedIn: true,
+      name: displayName,
+      email: email,
+      photo: photoURL
+    }
+    setUser(signedInUser);
+    console.log(user );
+    })
+    .catch((error) => {
+        // Handle Errors here.
+      console.log(error.code);
+      console.log(error.message);
+      // The email of the user's account used.
+      console.log(error.email);
+      // The firebase.auth.AuthCredential type that was used.
+      console.log(error.credential);
+    });
+  }
+const handleSignOut = () => {
+  firebase.auth().signOut()
+  .then((res) => {
+    const signOutUser = {
+      isSignedIn: false,
+      name: '',
+      email: '',
+      password: '',
+      photo: ''
+    }
+    setUser(signOutUser);
+    // Sign-out successful.
+  })
+  .catch((error) => {
+    // An error happened.
+  });
+}
+const handleBlur = (e) => {
+  let isFormValid = true;
+  if(e.target.name === 'email'){
+    const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
+    isFormValid = isEmailValid;
+
+  }
+  if(e.target.name === 'password'){
+    const isPasswordValid = e.target.value.length > 6;
+    const passwordHasNumber = /\d/.test(e.target.value);
+    isFormValid = isPasswordValid && passwordHasNumber;
+  }
+  if(isFormValid){
+    // [...cart, newItem]
+    const newUserInfo = {...user};
+    newUserInfo[e.target.name] = e.target.value;
+    setUser(newUserInfo);
+  }
+}
+
+const handleSubmit = () => {
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.isSignedIn ?
+        <button onClick={handleSignOut}>Sign Out</button> :
+        <button onClick={handleSignIn}>Sign In</button> 
+      }
+      {
+        user.isSignedIn && 
+        <div>
+          <p> WelCome, Mr. {user.name}</p>
+          <p> Your Email: {user.email}</p>
+          <img src={user.photo} alt=""/>
+        </div>
+      }
+      <h2>Authentication</h2>
+        <p>Name: {user.name}</p>
+        <p>Email: {user.email}</p>
+        <p>Password: {user.password}</p>
+
+      <form action="" onClick={handleSubmit}>
+        <input type="text" placeholder="Your name" name="name" onBlur={handleBlur}/> <br/>
+        <input onBlur={handleBlur} type="email" name="email" id="" placeholder="Enter Your Email"/> <br/>
+        <input onBlur={handleBlur} type="password" name="password" id="" placeholder="Enter Your Password"/>
+      </form>
+
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyByLqnVFssh0tvv6y756f78cCY-xfjfipo",
+   authDomain: "ema-jon-simple1.firebaseapp.com",
+   projectId: "ema-jon-simple1",
+   storageBucket: "ema-jon-simple1.appspot.com",
+   messagingSenderId: "806522689734",
+   appId: "1:806522689734:web:ac654ca1ce693f22e99fc7",
+   measurementId: "G-63EPVX1XSS"
+ };
+
+ export default firebaseConfig;
